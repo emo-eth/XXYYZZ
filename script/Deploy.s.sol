a0f4318add61f70905aa43c765c1767d624f8478
James Wenzel <jameswenzel@users.noreply.github.com>
2023-06-15 15:21:34 -0400

    add create2

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.8.17;
 
 import {Script, console2} from "forge-std/Script.sol";
 import {XXYYZZ} from "../src/XXYYZZ.sol";
+import {BaseCreate2Script} from "create2-helpers/script/BaseCreate2Script.s.sol";
 
-contract Deploy is Script {
+contract Deploy is BaseCreate2Script {
     function run() public {
-        vm.createSelectFork(getChain("goerli").rpcUrl);
-        uint256 pkey = vm.envUint("DEPLOYER_PRIVATE_KEY");
-        address deployer = vm.rememberKey(pkey);
+        vm.createSelectFork(getChain("mainnet").rpcUrl);
+        super.setUp();
         address creatorPayout = 0x7ade04fa0cbE2167E8bff758F48879BD0C6fFf92;
 
         uint24[] memory preMints = new uint24[](5);
@@ -18,16 +18,20 @@ contract Deploy is Script {
         preMints[4] = 0x00ff00;
         address seaDrop = 0x00005EA00Ac477B1030CE78506496e8C2dE24bf5;
 
-        vm.createSelectFork(getChain("goerli").rpcUrl);
+        bytes memory creationCode = type(XXYYZZ).creationCode;
 
-        vm.broadcast(deployer);
-        XXYYZZ test = new XXYYZZ(deployer,creatorPayout, 5, preMints,seaDrop);
-        uint256 mintPrice = test.MINT_PRICE();
+        creationCode = abi.encodePacked(creationCode, abi.encode(deployer, creatorPayout, 5, preMints, seaDrop));
+        console2.logBytes32(keccak256(creationCode)); // 0x441490b1ef05da3b8c59c58dbca8c7a9579225242dc8b957ce506bdd85e970cd
+        // starts with FF6000
+        uint256 salt = 64744444913995508200752332462249337323357634718575558519965188364118140729719;
+
+        // vm.broadcast(deployer);
+        address addy = _create2IfNotDeployed(deployer, bytes32(salt), creationCode);
+
+        // vm.broadcast(deployer);
+        XXYYZZ test = XXYYZZ(payable(addy)); //new XXYYZZ(deployer,creatorPayout, 5, preMints,seaDrop);
 
         vm.broadcast(deployer);
         test.configureSeaDrop();
-
-        vm.broadcast(deployer);
-        test.mint{value: mintPrice}();
     }
 }
